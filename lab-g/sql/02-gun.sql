d845b9b2b1130a1a405517b63b2c84acdbc5f1ff
Artur Zinchyn 57744 <dungeonmaster15tsss@gmail.com>
2026-06-13 18:15:22 +0200

    lab-i

--- lab-g/sql/02-gun.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE gun (
-                     id INTEGER PRIMARY KEY AUTOINCREMENT,
-                     name TEXT NOT NULL,
-                     caliber TEXT NOT NULL,
-                     magazine_capacity INTEGER NOT NULL
-);
